56f3fa9aa75586857f2985ce696cd6890791663b
AndressToscanom30 <02230131068@mail.udes.edu.co>
2026-05-05 18:33:03 -0500

    Primer commit, proyecto base creado con spring.initilzr

---
+++ src/main/java/com/cronos/gestiontributaria/GestionTributariaApplication.java
@@ -0,0 +1,13 @@
+package com.cronos.gestiontributaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTributariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTributariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cronos/gestiontributaria/GestionTributariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cronos.gestiontributaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTributariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
